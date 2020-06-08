7f4392974a969742a38b1ea7d4619221134ab3b2
이원주 <wonjubear251@partner.sk.com>
2020-06-08 14:52:13 +0900

    hg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewComponentComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
